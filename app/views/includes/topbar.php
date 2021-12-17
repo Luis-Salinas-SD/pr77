5f839015815cf1422fc12cf6fcf7a07b274345e9
Luis-Salinas-SD <paus.impala21@gmail.com>
2021-12-17 15:50:30 -0600

    Actulizacion para cris

--- app/views/includes/topbar.php
+++ app/views/includes/topbar.php
@@ -4,53 +4,57 @@
         <i data-feather="menu"></i>
     </a>
     <div class="navbar-content">
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
+        <div class="navbar-nav container-fluid d-flex justify-content-between">
+            <div class="nav-item">
+                <h4 class="text-warning">Empresa PR77</h4>
+            </div>
+            <div class="nav-item">
                 <h4 class="text-warning">Roberto Mejía</h4>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
-                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile">
-                </a>
-                <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
-                    <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
-                        <div class="mb-3">
-                            <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80" alt="">
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img class="wd-30 ht-30 rounded-circle" src="https://via.placeholder.com/30x30" alt="profile">
+                    </a>
+                    <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
+                        <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
+                            <div class="mb-3">
+                                <img class="wd-80 ht-80 rounded-circle" src="https://via.placeholder.com/80x80" alt="">
+                            </div>
+                            <div class="text-center">
+                                <p class="tx-16 fw-bolder">Amiah Burton</p>
+                                <p class="tx-12 text-muted">amiahburton@gmail.com</p>
+                            </div>
                         </div>
-                        <div class="text-center">
-                            <p class="tx-16 fw-bolder">Amiah Burton</p>
-                            <p class="tx-12 text-muted">amiahburton@gmail.com</p>
-                        </div>
+                        <ul class="list-unstyled p-1">
+                            <li class="dropdown-item py-2">
+                                <a href="pages/general/profile.html" class="text-body ms-0">
+                                    <i class="me-2 icon-md" data-feather="user"></i>
+                                    <span>Profile</span>
+                                </a>
+                            </li>
+                            <li class="dropdown-item py-2">
+                                <a href="javascript:;" class="text-body ms-0">
+                                    <i class="me-2 icon-md" data-feather="edit"></i>
+                                    <span>Edit Profile</span>
+                                </a>
+                            </li>
+                            <li class="dropdown-item py-2">
+                                <a href="javascript:;" class="text-body ms-0">
+                                    <i class="me-2 icon-md" data-feather="repeat"></i>
+                                    <span>Switch User</span>
+                                </a>
+                            </li>
+                            <li class="dropdown-item py-2">
+                                <a href="javascript:;" class="text-body ms-0">
+                                    <i class="me-2 icon-md" data-feather="log-out"></i>
+                                    <span>Log Out</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="list-unstyled p-1">
-                        <li class="dropdown-item py-2">
-                            <a href="pages/general/profile.html" class="text-body ms-0">
-                                <i class="me-2 icon-md" data-feather="user"></i>
-                                <span>Profile</span>
-                            </a>
-                        </li>
-                        <li class="dropdown-item py-2">
-                            <a href="javascript:;" class="text-body ms-0">
-                                <i class="me-2 icon-md" data-feather="edit"></i>
-                                <span>Edit Profile</span>
-                            </a>
-                        </li>
-                        <li class="dropdown-item py-2">
-                            <a href="javascript:;" class="text-body ms-0">
-                                <i class="me-2 icon-md" data-feather="repeat"></i>
-                                <span>Switch User</span>
-                            </a>
-                        </li>
-                        <li class="dropdown-item py-2">
-                            <a href="javascript:;" class="text-body ms-0">
-                                <i class="me-2 icon-md" data-feather="log-out"></i>
-                                <span>Log Out</span>
-                            </a>
-                        </li>
-                    </ul>
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
+
     </div>
 </nav>
